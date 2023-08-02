38722a48787760ae8986443a83c478b2fa71b901
aid003 <teremochek859@gmail.com>
2023-08-02 14:28:15 +0300

    fjgckjckyjgy

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ import {
 } from "./users/users.controller.js";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import { uploadPoster } from "./posters/posters.js";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -38,6 +39,7 @@ async function main() {
   app.use(express.urlencoded({ extended: true, limit: "4000000kb" }));
   app.use(cookieParser());
   app.use(morgan("tiny"));
+  app.use(express.static("/public"));
   app.use("/api/get-films/", getAllPublishedFilms);
   app.use("/api/get-film/", getFilmByTitle);
   app.use("/api/get-titles/", getAllTitles);
@@ -46,6 +48,7 @@ async function main() {
   app.use("/api/delete-all-published-films/", deleteAllPublishedFilms);
   app.use("/api/get-all-feilds/", getAllFields);
   app.use("/api/change-film-fields/", changeFildsToFilm);
+  app.use("/api/upload-films-poster-to-url/", uploadPoster);
 
   app.use("/films/", streamingVideo);
 
---
+++ posters/posters.js
@@ -0,0 +1,13 @@
+import asyncHandler from "express-async-handler";
+import path from "path";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const uploadPoster = asyncHandler(async (req, res) => {
+  const { posterUrl } = req.body;
+  res.sendFile(path.join(__dirname, "../public/posters", `${posterUrl}`));
+});
